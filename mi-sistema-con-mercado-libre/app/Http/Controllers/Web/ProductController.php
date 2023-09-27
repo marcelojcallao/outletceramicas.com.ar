e33d980b4c8f75b91d4dca39248dd11b0e98c616
Diego Barrueta <diego.barrueta@gmail.com>
2023-09-27 17:45:48 -0300

    view photos on site

--- mi-sistema-con-mercado-libre/app/Http/Controllers/Web/ProductController.php
+++ mi-sistema-con-mercado-libre/app/Http/Controllers/Web/ProductController.php
@@ -5,126 +5,126 @@ namespace App\Http\Controllers\Web;
 use App\Src\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Src\Tools\Constant;
 use App\Transformers\Products\ListProductsTransformer;
 use App\Transformers\Products\ProductAtWebTransformer;
 use App\Transformers\PublicationHere\ResultProductSearch;
 
 class ProductController extends Controller
 {
-    public function index()
-    {   
-        $productsAtWeb = Product::query();
+	public function index()
+	{
+		$productsAtWeb = Product::query();
 
-        $productsAtWeb =  $productsAtWeb = Product::whereHas('categories', function($q){
-            $q->where('active','=',1);//categoría activa
-        })->whereHas('pricelists', function($pl){
-            $pl->where('pricelist_id',4);//lista de precio para la tiend
-        });
-        
-        $productsAtWeb = $productsAtWeb->where('active', true)->where('published_here', true);
+		$productsAtWeb =  $productsAtWeb = Product::whereHas('categories', function ($q) {
+			$q->where('active', '=', 1); //categoría activa
+		})->whereHas('pricelists', function ($pl) {
+			$pl->where('pricelist_id', Constant::WEB_PRICE_LIST); //lista de precio para la tiend
+		});
 
-        if (request()->product_name) {
-        
-            $name = request()->product_name;
-            $productsAtWeb = $productsAtWeb->where('name', 'like', "%{$name}%")
-                ->orWhere('code', 'like', "%{$name}%");
-        }
-        
-        if (request()->has('by_category')) {
-            
-            if(! (request()->by_category == 'ALL-CATEGORIES')){
-                $category_code = request()->by_category;
-                $productsAtWeb = $productsAtWeb->where('code', 'like', "%{$category_code}%");
-            }
-        }
+		$productsAtWeb = $productsAtWeb->where('active', true)->where('published_here', true);
 
-        if (request()->sort == 'name_a_z') {
-            $productsAtWeb = $productsAtWeb->orderBy('name', 'asc');
-        }
+		if (request()->product_name) {
 
-        if (request()->sort == 'name_z_z') {
-            $productsAtWeb = $productsAtWeb-> orderByDesc('name');
-        }
+			$name = request()->product_name;
+			$productsAtWeb = $productsAtWeb->where('name', 'like', "%{$name}%")
+				->orWhere('code', 'like', "%{$name}%");
+		}
 
-        $productsAtWeb = $productsAtWeb->paginate(12);
-        
-        $pagination = [
-            'total' => $productsAtWeb->total(),
-            'per_page' => $productsAtWeb->perPage(),
-            'current_page' => $productsAtWeb->currentPage(),
-            'last_page' => $productsAtWeb->lastPage(),
-            'from' => $productsAtWeb->firstItem(),
-            'to' => $productsAtWeb->lastItem()
-        ];
+		if (request()->has('by_category')) {
 
-        $products = fractal()
-            ->collection($productsAtWeb, new ProductAtWebTransformer())->toArray()['data'];
+			if (!(request()->by_category == 'ALL-CATEGORIES')) {
+				$category_code = request()->by_category;
+				$productsAtWeb = $productsAtWeb->where('code', 'like', "%{$category_code}%");
+			}
+		}
 
-        $response = [
-            'pagination' => $pagination,
-            'data' => $products
-        ];
+		if (request()->sort == 'name_a_z') {
+			$productsAtWeb = $productsAtWeb->orderBy('name', 'asc');
+		}
 
-        return response()->json($response, 200);
-    }
+		if (request()->sort == 'name_z_z') {
+			$productsAtWeb = $productsAtWeb->orderByDesc('name');
+		}
 
-    public function by_category(Request $request)
-    {
-        $slug = $request->slug;
+		$productsAtWeb = $productsAtWeb->paginate(12);
 
-        //$post = Post::whereSlug($slugString)->get();
-        
-        $productsAtWeb = Product::whereHas('categories', function($q) use($slug){
-            $q->where('slug', $slug);
-        })->get();
+		$pagination = [
+			'total' => $productsAtWeb->total(),
+			'per_page' => $productsAtWeb->perPage(),
+			'current_page' => $productsAtWeb->currentPage(),
+			'last_page' => $productsAtWeb->lastPage(),
+			'from' => $productsAtWeb->firstItem(),
+			'to' => $productsAtWeb->lastItem()
+		];
 
-        $products = fractal()
-            ->collection($productsAtWeb, new ListProductsTransformer())
-            ->toArray()['data'];
-            
-        return response()->json($products, 200);
-    }
+		$products = fractal()
+			->collection($productsAtWeb, new ProductAtWebTransformer())->toArray()['data'];
+
+		$response = [
+			'pagination' => $pagination,
+			'data' => $products
+		];
+
+		return response()->json($response, 200);
+	}
+
+	public function by_category(Request $request)
+	{
+		$slug = $request->slug;
+
+		//$post = Post::whereSlug($slugString)->get();
+
+		$productsAtWeb = Product::whereHas('categories', function ($q) use ($slug) {
+			$q->where('slug', $slug);
+		})->get();
+
+		$products = fractal()
+			->collection($productsAtWeb, new ListProductsTransformer())
+			->toArray()['data'];
+
+		return response()->json($products, 200);
+	}
 
 
-    public function show(Request $request)
-    {
-        $productOnSale = Product::where('id', $id)->get();
+	public function show(Request $request)
+	{
+		$productOnSale = Product::where('id', $id)->get();
 
-        $product = fractal()
-            ->collection($productOnSale, new ListProductsTransformer())
-            ->toArray()['data'];
-            
-        return response()->json($product, 200);
-    }
+		$product = fractal()
+			->collection($productOnSale, new ListProductsTransformer())
+			->toArray()['data'];
 
-    public function search_products(Request $request)
-    {
-        /* $s = explode(" ", $request->search);
+		return response()->json($product, 200);
+	}
+
+	public function search_products(Request $request)
+	{
+		/* $s = explode(" ", $request->search);
 
         $search = collect($s)->map(function($i){
             return '*' . $i . '*';
         })->toArray(); */
-        
-        $products = Product::where('name', 'like', '%' . $request->search . '%')
-            ->paginate(50);
 
-        $products_results = fractal()
-                ->collection($products, new ProductAtWebTransformer())
-                ->toArray()['data'];
+		$products = Product::where('name', 'like', '%' . $request->search . '%')
+			->paginate(50);
 
-        $response = [
-            'pagination' => [
-                'total' => $products->total(),
-                'per_page' => $products->perPage(),
-                'current_page' => $products->currentPage(),
-                'last_page' => $products->lastPage(),
-                'from' => $products->firstItem(),
-                'to' => $products->lastItem()
-            ],
-            'data' => $products_results
-        ];
+		$products_results = fractal()
+			->collection($products, new ProductAtWebTransformer())
+			->toArray()['data'];
 
-        return response()->json($response, 200);
-    }
+		$response = [
+			'pagination' => [
+				'total' => $products->total(),
+				'per_page' => $products->perPage(),
+				'current_page' => $products->currentPage(),
+				'last_page' => $products->lastPage(),
+				'from' => $products->firstItem(),
+				'to' => $products->lastItem()
+			],
+			'data' => $products_results
+		];
 
+		return response()->json($response, 200);
+	}
 }
--- mi-sistema-con-mercado-libre/app/Transformers/Products/ProductAtWebTransformer.php
+++ mi-sistema-con-mercado-libre/app/Transformers/Products/ProductAtWebTransformer.php
@@ -3,48 +3,48 @@
 namespace App\Transformers\Products;
 
 use App\Src\Models\Product;
+use App\Src\Tools\Constant;
 use League\Fractal\TransformerAbstract;
 
 class ProductAtWebTransformer extends TransformerAbstract
 {
-    public function getPictures($product)
-    {
-        if ($product->getMedia('products')->isNotEmpty()) 
-        {
-            return $product->getMedia('products')->map(function($img){
-                return $img->getUrl();
-            })->toArray();
-        }
+	public function getPictures($product)
+	{
+		if ($product->getMedia('products')->isNotEmpty()) {
+			return $product->getMedia('products')->map(function ($img) {
+				return $img->getUrl();
+			})->toArray();
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public function getPrice($p)
-    {
-        if ($p->see_price_on_the_web) {
-            return $p->pricelists->filter(function($q){
-                return $q->id == 4;
-            })->first()->pivot->price;
-        }
+	public function getPrice($p)
+	{
+		if ($p->see_price_on_the_web) {
+			return $p->pricelists->filter(function ($q) {
+				return $q->id == Constant::WEB_PRICE_LIST;
+			})->first()->pivot->price;
+		}
 
-        return 0;
-    }
+		return 0;
+	}
 
-    public function transform( Product $product)
-    {
-        return [
-            'id' => $product->id,
-            'code' => strtoupper($product->code),
-            'name' => strtoupper($product->name),
-            'description' => $product->description,
-            'attributes' => $product->attributes,
-            'stock' => $product->stock,
-            //WWW' => $product->priority->name,
-            'slug' => $product->slug,
-            'hasPrice' => $product->see_price_on_the_web,
-            //'price' => $product->pricelists,
-            'price' => $this->getPrice($product),
-            'pictures' => $this->getPictures($product)
-        ];
-    }
+	public function transform(Product $product)
+	{
+		return [
+			'id' => $product->id,
+			'code' => strtoupper($product->code),
+			'name' => strtoupper($product->name),
+			'description' => $product->description,
+			'attributes' => $product->attributes,
+			'stock' => $product->stock,
+			//WWW' => $product->priority->name,
+			'slug' => $product->slug,
+			'hasPrice' => $product->see_price_on_the_web,
+			//'price' => $product->pricelists,
+			'price' => $this->getPrice($product),
+			'pictures' => $this->getPictures($product)
+		];
+	}
 }
