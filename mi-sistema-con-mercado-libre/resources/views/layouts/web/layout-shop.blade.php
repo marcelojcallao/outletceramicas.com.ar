2862c49f3bfd30f3d55c1e6d1277d5f28ccaf7b4
Diego Barrueta <diego.barrueta@gmail.com>
2023-10-10 14:27:23 -0300

    edit product

--- mi-sistema-con-mercado-libre/resources/views/layouts/web/layout-shop.blade.php
+++ mi-sistema-con-mercado-libre/resources/views/layouts/web/layout-shop.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html> {{-- /Tienda --}}
 <html lang="en">
+
 <head>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164132632-1"></script>
@@ -14,27 +15,28 @@
 	<meta charset="utf-8">
 	<title>{{config('app.name')}}</title>
 	<meta name="keywords" content="HTML5 Template">
-	<meta name="description" content="Asaliah - Perfiles, Chapas, Corralón de Materiales - Venta por Mayor y Menor">
+	<meta name="description" content="Outlet Cerámicos - Venta por mayor y menor">
 	<meta name="author" content=DIMAiT>
 	<link rel="shortcut icon" href="/images/logos/favicon.ico">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<link rel="stylesheet" href="/css/theme.css">
 </head>
+
 <body>
 	<div id="app">
 		@if (\Request::is('tienda'))
-			<div id="loader-wrapper">
-				<div id="loader">
-					<div class="dot"></div>
-					<div class="dot"></div>
-					<div class="dot"></div>
-					<div class="dot"></div>
-					<div class="dot"></div>
-					<div class="dot"></div>
-					<div class="dot"></div>
-				</div>
+		<div id="loader-wrapper">
+			<div id="loader">
+				<div class="dot"></div>
+				<div class="dot"></div>
+				<div class="dot"></div>
+				<div class="dot"></div>
+				<div class="dot"></div>
+				<div class="dot"></div>
+				<div class="dot"></div>
 			</div>
+		</div>
 		@endif
 		<header>
 			<!-- tt-mobile menu -->
@@ -42,7 +44,7 @@
 			<!-- <mobile_menu /> -->
 			<!-- tt-mobile-header -->
 			@include('layouts.web.partials.mobile-header')
-			
+
 			<!-- tt-desktop-header -->
 			@include('layouts.web.partials.desktop-header')
 
@@ -69,33 +71,33 @@
 		</div> -->
 		<div id="tt-pageContent" style="min-height:500px; background-color:#dbd8e3;padding-bottom:2rem;padding-top:1rem">
 			@if (\Request::is('tienda'))
-				<div class="container-indent">
-					<div class="container">
-						<div class="row">
-							@include('layouts.web.partials.left-column')
-							@yield('main-content')
-						</div>
+			<div class="container-indent">
+				<div class="container">
+					<div class="row">
+						@include('layouts.web.partials.left-column')
+						@yield('main-content')
 					</div>
 				</div>
+			</div>
 			@else
-				@yield('main-content')
+			@yield('main-content')
 			@endif
-			
+
 		</div>
 		<footer>
 			@include('layouts.web.partials.footer')
 		</footer>
 		<a href="#" class="tt-back-to-top">BACK TO TOP</a>
 		<!-- modal (AddToCartProduct) -->
-		<div class="modal  fade"  id="modalAddToCartProduct" tabindex="-1" role="dialog" aria-label="myModalLabel" aria-hidden="true">
+		<div class="modal  fade" id="modalAddToCartProduct" tabindex="-1" role="dialog" aria-label="myModalLabel" aria-hidden="true">
 			@include('layouts.web.partials.modal-add-to-cart-product')
 		</div>
 		<!-- modal (quickViewModal) -->
-		<div class="modal  fade"  id="ModalquickView" tabindex="-1" role="dialog" aria-label="myModalLabel" aria-hidden="true">
+		<div class="modal  fade" id="ModalquickView" tabindex="-1" role="dialog" aria-label="myModalLabel" aria-hidden="true">
 			@include('layouts.web.partials.modal-quick-view')
 		</div>
 		<!-- modalVideoProduct -->
-		<div class="modal fade"  id="modalVideoProduct" tabindex="-1" role="dialog" aria-label="myModalLabel" aria-hidden="true">
+		<div class="modal fade" id="modalVideoProduct" tabindex="-1" role="dialog" aria-label="myModalLabel" aria-hidden="true">
 			<div class="modal-dialog modal-video">
 				<div class="modal-content ">
 					<div class="modal-header">
@@ -110,7 +112,7 @@
 			</div>
 		</div>
 		<!-- modal (ModalSubsribeGood) -->
-		<div class="modal  fade"  id="ModalSubsribeGood" tabindex="-1" role="dialog" aria-label="myModalLabel" aria-hidden="true">
+		<div class="modal  fade" id="ModalSubsribeGood" tabindex="-1" role="dialog" aria-label="myModalLabel" aria-hidden="true">
 			<div class="modal-dialog modal-xs">
 				<div class="modal-content ">
 					<div class="modal-header">
@@ -126,21 +128,22 @@
 		</div>
 	</div>
 	@if (\Request::is('tienda'))
-		<script src="/external/jquery/jquery.min.js"></script>
-		<script src="/external/bootstrap/js/bootstrap.min.js"></script>
-		<script src="/js/app.js"></script>
-		<script src="/external/slick/slick.min.js"></script>
-		<script src="/external/panelmenu/panelmenu.js"></script>
-		<script src="/external/countdown/jquery.plugin.min.js"></script>
-		<script src="/external/countdown/jquery.countdown.min.js"></script>
-		<script src="/external/lazyLoad/lazyload.min.js"></script>
-		<script src="/js/main.js"></script>
-		<!-- form validation and sending to mail -->
-		{{-- <script src="/external/form/jquery.form.js"></script>
+	<script src="/external/jquery/jquery.min.js"></script>
+	<script src="/external/bootstrap/js/bootstrap.min.js"></script>
+	<script src="/js/app.js"></script>
+	<script src="/external/slick/slick.min.js"></script>
+	<script src="/external/panelmenu/panelmenu.js"></script>
+	<script src="/external/countdown/jquery.plugin.min.js"></script>
+	<script src="/external/countdown/jquery.countdown.min.js"></script>
+	<script src="/external/lazyLoad/lazyload.min.js"></script>
+	<script src="/js/main.js"></script>
+	<!-- form validation and sending to mail -->
+	{{-- <script src="/external/form/jquery.form.js"></script>
 		<script src="/external/form/jquery.validate.min.js"></script>
 		<script src="/external/form/jquery.form-init.js"></script> --}}
 	@else
-		@yield('scripts')
+	@yield('scripts')
 	@endif
 </body>
+
 </html>
